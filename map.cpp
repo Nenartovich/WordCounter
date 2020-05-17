297accd44ae43014970f80c93db1a3810b420df4
nenartovich <evgeny.nenart@gmail.com>
2020-05-17 20:54:18 +0300

    Fixed version

--- map.cpp
+++ map.cpp
@@ -1,29 +1,15 @@
 #include <bits/stdc++.h>
 
 int main() {
+    std::string key;
     std::string line;
-    while (getline(std::cin, line)) {
-        std::string tmp = "";
-        int cnt = 0;
-        for (int i = 0; i < line.size(); i++) {
-            if (line[i] != '\t') {
-                tmp += line[i];
-            } else {
-                if (tmp != "") {
-                    if (cnt == 0 && (tmp == "Key" || tmp == "key")) {
-                        tmp = "";
-                        continue;
-                    } else {
-                        cnt++;
-                        std::cout << tmp << "\t1" << std::endl;
-                    }
-                    tmp = "";
-                }
-            }
-        }
-
-        if (tmp != "") {
-            std::cout << tmp << "\t1" << std::endl;
+    while (std::getline(std::cin, key, '\t')) {
+        std::getline(std::cin, line);
+        std::stringstream words;
+        words << line;
+        std::string word;
+        while (words >> word) {
+            std::cout << word << "\t1" << std::endl;
         }
     }
     return 0;
--- mapreduce.cpp
+++ mapreduce.cpp
@@ -2,74 +2,52 @@
 #include <boost/process.hpp>
 
 namespace bp = boost::process;
+const std::string DIRNAME = "temporary_files_directory/";
 
-bool cmp(std::pair<std::string, int> first_pair,
-         std::pair<std::string, int> second_pair) {
-    return ((first_pair.first < second_pair.first) ||
-            (first_pair.first == second_pair.first && first_pair.second < second_pair.second));
-}
-
-void Reduce(std::string script_path, std::string file_name) {
+void Reduce(const std::string& script_path, const std::string& file_name) {
     std::string new_file_name = file_name;
     new_file_name[0] = 'b';
-    bp::system(script_path, bp::std_in < file_name, bp::std_out > new_file_name);
+    bp::system(script_path, bp::std_in < (DIRNAME + file_name), bp::std_out > (DIRNAME + new_file_name));
 }
 
-void CreateFile(const std::vector<std::pair<std::string, int>>& data, std::string file_name) {
-    std::ofstream output_stream(file_name);
-    for (int i = 0; i < data.size(); i++) {
-        output_stream << data[i].first << '\t' << data[i].second << std::endl;
+void CreateFile(const std::pair<std::string, std::vector<int>>& data, const std::string& file_name) {
+    std::ofstream output_stream(DIRNAME + file_name);
+    for (const int& value : data.second) {
+        output_stream << data.first << '\t' << value << std::endl;
     }
 }
 
-void MergeFiles(int files_count, std::string destination_file) {
+void MergeFiles(int files_count, const std::string& destination_file) {
     std::ofstream output_stream(destination_file);
     std::string key; int value;
     for (int i = 1; i < files_count; i++) {
-        std::ifstream input_stream2("b" + std::to_string(i) + ".txt");
+        std::ifstream input_stream2(DIRNAME + "b" + std::to_string(i) + ".txt");
         input_stream2 >> key >> value;
         output_stream << key << '\t' <<  value << '\n';
     }
 }
 
-int FileSpliting(std::string script_path, std::string source_file, std::string destination_file) {
-    std::vector<std::pair<std::string, int>> pairs;
+int FileSpliting(const std::string& script_path, const std::string& source_file, const std::string& destination_file) {
+    std::map<std::string, std::vector<int>> pairs;
     std::string word;
     int num;
     std::ifstream input_stream(source_file);
     while (input_stream >> word) {
         input_stream >> num;
-        pairs.push_back({word, num});
+        pairs[word].push_back(num);
     }
-
-    std::sort(pairs.begin(), pairs.end(), cmp);
 
     int cnt = 1;
-    std::vector<std::pair<std::string, int>> v;
-
-    v.push_back(pairs[0]);
-    for (int i = 1; i < pairs.size(); i++) {
-        if (v.back().first == pairs[i].first) {
-            v.push_back(pairs[i]);
-        } else {
-            std::string file_name = "a" + std::to_string(cnt) + ".txt";
-            cnt++;
-            CreateFile(v, file_name);
-            v.clear();
-            v.push_back(pairs[i]);
-        }
-    }
-
-    if (!v.empty()) {
+    for (auto& x: pairs) {
         std::string file_name = "a" + std::to_string(cnt) + ".txt";
-        CreateFile(v, file_name);
         cnt++;
+        CreateFile(x, file_name);
     }
 
     return cnt;
 }
 
-void Map(std::string script_path, std::string source_file, std::string destination_file) {
+void Map(const std::string& script_path, const std::string& source_file, const std::string& destination_file) {
     bp::system(script_path, bp::std_in < source_file, bp::std_out > destination_file);
 }
 
@@ -79,11 +57,13 @@ int main(int n, char* args[]) {
         return 0;
     }
 
-    if ((std::string)args[1] == "map") {
-        Map((std::string) args[2], (std::string) args[3], (std::string) args[4]);
-    } else if ((std::string)args[1] == "reduce") {
-        int file_count = FileSpliting((std::string) args[2], (std::string) args[3],
-                (std::string) args[4]);
+    if (std::string(args[1]) == "map") {
+        Map(std::string(args[2]) , std::string(args[3]), std::string(args[4]));
+    } else if (std::string(args[1]) == "reduce") {
+        bp::system("mkdir " + DIRNAME);
+
+        int file_count = FileSpliting(std::string(args[2]), std::string(args[3]),
+                std::string(args[4]));
 
         for (int i = 1; i < file_count; i++) {
             Reduce(args[2], "a" + std::to_string(i) + ".txt");
@@ -91,11 +71,7 @@ int main(int n, char* args[]) {
 
         MergeFiles(file_count, args[4]);
 
-        for (int i = 1; i < file_count; i++) {
-            std::string tmp = std::to_string(i) + ".txt";
-            bp::system("unlink a" + tmp);
-            bp::system("unlink b" + tmp);
-        }
+        bp::system("rm -rf " + DIRNAME);
 
     } else {
         std::cerr << "Wrong operation" << std::endl;
